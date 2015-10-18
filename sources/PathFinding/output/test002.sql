33150542499d3cddad2432cd0614fd2090a25ee3
VasilijP <peter@way-to-play.eu>
2015-10-18 20:41:56 +0200

    2005-08-31 version of PathFinding

--- sources/PathFinding/output/test002.sql
+++ sources/PathFinding/output/test002.sql
@@ -1,36 +1,72 @@
 -- Test finished
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
-VALUES('bellmanfordlimit',
+VALUES('dijkstra',
                             1000,
-                           39185,
+                           22243,
                               25,
                               40,
                       1125147001,
-                        25273971,
+                        17696421,
                        'test002',
-      '(682)246,376,518,482,896');
+'(844)86,51,368,993,975,514,241,204,13,992,320,530,534');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                            1000,
+                           22243,
+                              25,
+                              40,
+                      1125147001,
+                        17643461,
+                       'test002',
+'(844)86,51,368,993,975,514,241,204,13,992,320,530,534');
 
 -- Test finished
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
 VALUES('dijkstraheap',
                             1000,
-                           39185,
+                           22243,
                               25,
                               40,
                       1125147001,
-                        22797720,
+                        16007827,
                        'test002',
-      '(682)246,376,518,482,896');
+'(844)86,51,368,993,975,514,241,204,13,992,320,530,534');
 
 -- Test finished
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
 VALUES('bellmanfordlimit',
+                            1000,
+                           22243,
+                              25,
+                              40,
+                      1125147001,
+                        17541842,
+                       'test002',
+'(844)86,51,368,993,975,514,241,204,13,992,320,530,534');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstra',
                             2500,
                            99137,
                               25,
                               40,
                       1125147001,
-                        78118047,
+                        98763408,
+                       'test002',
+       '(396)2424,1875,834,2492');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                            2500,
+                           99137,
+                              25,
+                              40,
+                      1125147001,
+                        93339729,
                        'test002',
        '(396)2424,1875,834,2492');
 
@@ -42,19 +78,43 @@ VALUES('dijkstraheap',
                               25,
                               40,
                       1125147001,
-                        77260905,
+                        82232023,
                        'test002',
        '(396)2424,1875,834,2492');
 
 -- Test finished
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
 VALUES('bellmanfordlimit',
+                            2500,
+                           99137,
+                              25,
+                              40,
+                      1125147001,
+                        78742526,
+                       'test002',
+       '(396)2424,1875,834,2492');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstra',
                             5000,
                            82744,
                               25,
                               30,
                       1125147001,
-                        94193469,
+                        98442000,
+                       'test002',
+'(361)2852,2877,3491,4263,171,4637,2316,4087,2697,3815,1246');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                            5000,
+                           82744,
+                              25,
+                              30,
+                      1125147001,
+                       148626360,
                        'test002',
 '(361)2852,2877,3491,4263,171,4182,4924,4087,2697,3815,1246');
 
@@ -66,7 +126,7 @@ VALUES('dijkstraheap',
                               25,
                               30,
                       1125147001,
-                        86134320,
+                        85936086,
                        'test002',
 '(361)2852,2877,3491,4263,171,4637,2316,4087,2697,3815,1246');
 
@@ -74,23 +134,155 @@ VALUES('dijkstraheap',
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
 VALUES('bellmanfordlimit',
                             5000,
-                           57137,
+                           82744,
+                              25,
+                              30,
+                      1125147001,
+                        94438075,
+                       'test002',
+'(361)2852,2877,3491,4263,171,4182,4924,4087,2697,3815,1246');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstra',
+                           10000,
+                          199778,
                               25,
                               20,
                       1125147001,
-                        65300769,
+                       570928230,
                        'test002',
-'(878)2342,3961,3339,1580,923,1502,1130,4256,1397,1575,2501,1912');
+  '(863)7192,824,6293,4319,2579');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                           10000,
+                          199778,
+                              25,
+                              20,
+                      1125147001,
+                       306090954,
+                       'test002',
+  '(863)7192,824,6293,4319,2579');
 
 -- Test finished
 INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
 VALUES('dijkstraheap',
-                            5000,
-                           57137,
+                           10000,
+                          199778,
                               25,
                               20,
                       1125147001,
-                        57610224,
+                       254865987,
                        'test002',
-'(878)2342,3961,3339,1580,923,1502,1130,4256,1397,1575,2501,1912');
+  '(863)7192,824,6293,4319,2579');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanfordlimit',
+                           10000,
+                          199778,
+                              25,
+                              20,
+                      1125147001,
+                       270341928,
+                       'test002',
+  '(863)7192,824,6293,4319,2579');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstra',
+                           20000,
+                          499682,
+                              25,
+                              25,
+                      1125147001,
+                      4193414739,
+                       'test002',
+    '(1243)4796,8106,6746,16809');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                           20000,
+                          499682,
+                              25,
+                              25,
+                      1125147001,
+                       931833612,
+                       'test002',
+    '(1243)4796,8106,6746,16809');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstraheap',
+                           20000,
+                          499682,
+                              25,
+                              25,
+                      1125147001,
+                       865157895,
+                       'test002',
+    '(1243)4796,8106,6746,16809');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanfordlimit',
+                           20000,
+                          499682,
+                              25,
+                              25,
+                      1125147001,
+                       829780659,
+                       'test002',
+    '(1243)4796,8106,6746,16809');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstra',
+                           30000,
+                         1199210,
+                              25,
+                              40,
+                      1125147001,
+                     16535125890,
+                       'test002',
+'(894)25881,9406,16161,28457,17680');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanford',
+                           30000,
+                         1199210,
+                              25,
+                              40,
+                      1125147001,
+                      2367561717,
+                       'test002',
+'(894)25881,9406,16161,28457,17680');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('dijkstraheap',
+                           30000,
+                         1199210,
+                              25,
+                              40,
+                      1125147001,
+                      2181383973,
+                       'test002',
+'(894)25881,9406,16161,28457,17680');
+
+-- Test finished
+INSERT INTO result(alg,vertcount,edgecount,elenadd,conn,task,cycles,batchname,result) 
+VALUES('bellmanfordlimit',
+                           30000,
+                         1199210,
+                              25,
+                              40,
+                      1125147001,
+                      2184122394,
+                       'test002',
+'(894)25881,9406,16161,28457,17680');
 
--- sources/PathFinding/solutions.cpp
+++ sources/PathFinding/solutions.cpp
@@ -1,24 +1,37 @@
 
 #include <stdlib.h>
 #include <limits.h>
+#include <math.h>
+#include "utils.h"
 #include "solutions.h"
 
 
 SOLUTION* solveEmpty(const JOB * j);//empty implementation
-SOLUTION* solveDijkstra(const JOB * j);//dijkstra shortest pathfinding algorithm
 SOLUTION* solveBellmanFord(const JOB * j);//bellman ford shortest pathfinding algorithm
-SOLUTION* solveDijkstraHeap(const JOB * j);//dijkstra shortest pathfinding algorithm + heap
 SOLUTION* solveBellmanFordLimit(const JOB * j);//bellman ford s upravou (orezanie prehladavania podla doposial najdenej cesty)
+SOLUTION* solveBellmanFordGeom(const JOB * j);//bellman ford s upravou (orezanie prehladavania podla geometrickych informacii)
+SOLUTION* solveDijkstra(const JOB * j);//dijkstra shortest pathfinding algorithm
+SOLUTION* solveDijkstraHeap(const JOB * j);//dijkstra shortest pathfinding algorithm + heap
+SOLUTION* solveDijkstraHeapGeom(const JOB * j);//dijkstra shortest pathfinding algorithm + heap + use of geometric informations
+
 
 /*  Always update these fields together  */
-/**/unsigned int ALGCOUNT = 5;								//number of algorithms in this unit
+/**/unsigned int ALGCOUNT = 7;								//number of algorithms in this unit
 /**/
-/**/char* AlgNames[] = {"empty","dijkstra","bellmanford","dijkstraheap","bellmanfordlimit"};//array of names
+/**/char* AlgNames[] = {"empty",
+						"dijkstra",
+						"bellmanford",
+						"dijkstraheap",
+						"bellmanfordlimit",
+						"dijkstraheapgeom",
+						"bellmanfordgeom"};//array of names
 /**/SOLUTION* (*(Algs[]))(const JOB *) = {solveEmpty,
 										  solveDijkstra,
 										  solveBellmanFord,
 										  solveDijkstraHeap,
-										  solveBellmanFordLimit
+										  solveBellmanFordLimit,
+										  solveDijkstraHeapGeom,
+										  solveBellmanFordGeom
 										};	//array of func pointers (according to AlgNames)
 /*****************************************/
 
@@ -269,6 +282,153 @@ SOLUTION* solveDijkstraHeap(const JOB * j){
 
 	return(rv);
 }
+
+SOLUTION* solveDijkstraHeapGeom(const JOB * j){//s vyuzitim geometrickych udajov
+	int * Vs = new int[j->vertNo+1];	//index kde zacina zoznam nasledovnikov vo Vi
+	int * Vc = new int[j->vertNo+1];	//pocet zaznamov vo Vi pre dany vrchol
+	int * Vp = new int[j->vertNo+1];	//predchodcovia na ceste zo src
+	int * Vm = new int[j->vertNo+1];	//minimalna dlzka cesty spocitana ako eukl. vzdialenost
+	int * Vi = new int[(j->edgeNo)*2];	//zoznam naslednikov, naslednici pre i zacinaju od Vi[Vs[i]]
+	int * Vl = new int[(j->edgeNo)*2];	//dlzky hran
+	
+
+	int src = j->src;					//pociatok
+	int dst = j->dst;					//koniec
+
+	int cumc = 0;						//kumulativne cetnosti
+	int hilimit = j->vertNo+1;	
+	for (int rpc = 1; rpc< hilimit; rpc++ ) {
+		Vs[rpc] = cumc;					//zoznam zacina v poli Vi na indexe cumc
+		Vc[rpc] = 0;					//pocet naslednikov ulozeny pre kazdy vrchol je zatial 0
+		cumc+=j->V[rpc].deg;			//posun o stupen vrchola
+	}
+	int dx=j->V[j->dst].x, dy=j->V[j->dst].y, dz=j->V[j->dst].z;
+	int rx,ry,rz;
+	for (int rpc = 1; rpc<hilimit; rpc++){		//spocitanie minimalnych vzdialenosti
+		rx = j->V[rpc].x - dx;
+		ry = j->V[rpc].y - dy;
+		rz = j->V[rpc].z - dz;
+		Vm[rpc] = int(floor(sqrt(double(rx*rx+ry*ry+rz*rz))));	//ulozene ako druha odmocnina
+	}
+
+	int v1,v2;	
+	hilimit=j->edgeNo;
+	for (int rpc=0; rpc< hilimit; rpc++){
+		EDGE e = j->E[rpc];
+		v1 = e.v1;
+		v2 = e.v2;
+		Vl[Vs[v1]+Vc[v1]]=e.l;			//dlzka cesty
+		Vi[Vs[v1]+Vc[v1]++]=v2;			//v1 prida do zoznamu naslednikov v2 a zvysi pocet vo Vc[v1]
+		Vl[Vs[v2]+Vc[v2]]=e.l;
+		Vi[Vs[v2]+Vc[v2]++]=v1;
+	}
+
+	int * Vd =  new int[j->vertNo+1];	//vzdialenost do vrchola z source, INT_MAX pre neprebrany	
+	int * H = new int[j->vertNo+1];		//indexuje sa od 1, 0 v Hp = ze tam nieje
+	int * Hp = new int[j->vertNo+1];	//pozicie vrcholov v heape
+	H[1] = src;							//najprv je tam len src
+	hilimit = j->vertNo+1;
+	for (int rpc=0; rpc<hilimit;rpc++){ Vd[rpc] = INT_MAX; Hp[rpc]=0; }
+	Hp[src] = 1;
+	int Hc = 1;							//pocet hodnot v heape			
+
+	Vd[src]=0;
+	
+	int vminI;							//index vrchola na prebranie
+	while (Hc){							//kym nieje vrchol V[dst] prebrany a Fronta je neprazdna
+		/*vyber minima z heapu*/
+		vminI = H[1];
+		Hp[vminI] = 0;				
+		/**/
+		if (vminI==dst) break;			//uz je preberany cielovy vrchol, takze sa lepsia cesta donho nenajde, mozme skoncit		
+		//oprava struktury heapu		
+		Hc--;							//ak bol vminI == dst, skonci sa uz skor a v Fc je aspon 1, podla toho sa rozlisi ci sa nasla cesta
+		if (Hc){
+			int X = H[Hc+1];
+			int j = 1;
+			int n;
+			bool pokracovat = 2<=Hc;
+			while (pokracovat) {
+				n=2*j;
+				n+=((n<Hc)&&(Vd[H[n+1]]<Vd[H[n]]));
+				if (Vd[X]>Vd[H[n]]){
+					H[j] = H[n];
+					Hp[H[j]] = j;
+					j=n;
+					pokracovat = 2*j <= Hc;
+				} else pokracovat = false;							
+			}
+			H[j] = X;
+			Hp[X] = j;
+		}
+		if (Vd[vminI]+Vm[vminI] > Vd[dst]) continue;//ak je vzdialenost vrchola + minimalna dlzka cesty do ciela vacsia ako sucasna, vynechame vrchol
+
+		hilimit = Vs[vminI]+Vc[vminI];
+		for (int rpc=Vs[vminI]; rpc< hilimit; rpc++)
+			if (Vd[Vi[rpc]]>Vd[vminI]+Vl[rpc]){
+				Vd[Vi[rpc]]=Vd[vminI]+Vl[rpc];
+				Vp[Vi[rpc]]=vminI;
+				//pridat alebo upravit poziciu Vi[rpc] v heape pretoze sa zmenila vzdialenost
+				int j = Hp[Vi[rpc]];
+				int p;
+				int X = Vi[rpc];
+				if (!j)//ak tam nie je treba ho pridat
+				 j = ++Hc;
+				bool pokracovat = j > 1;
+				while (pokracovat){
+					p = j / 2;
+					if (Vd[X] < Vd[H[p]]){
+						H[j] = H[p];
+						Hp[H[j]] = j;
+						j=p;
+						pokracovat = j>1;
+					} else pokracovat = false;										
+				}				
+				H[j] = X;
+				Hp[X] = j;
+			}		
+	}
+
+	/*navrat riesenia*/
+	SOLUTION * rv = new SOLUTION;
+	if (!Hc) {							//ak to skoncilo vyprazdnenim fronty
+		rv->exists=false;
+		rv->l=0;
+		rv->V=NULL;
+		rv->vertNo=0;
+	} else {
+		//vytvorenie navratovej hodnoty v tvare: (dlzkacesty)src,v1,v2,...,vn,dst
+		rv->exists = true;
+		rv->l = Vd[dst];
+		int i = dst,count = 1;
+		do {
+			i = Vp[i];
+			count++;
+		} while (i!=src);
+		rv->vertNo = count;
+		rv->V = new unsigned int[count];
+		unsigned int * P = rv->V;	
+		i = dst;
+		P[--count] = dst;
+		do {		
+			i = Vp[i];
+			P[--count] = i;
+		} while (i!=src);
+	}
+	//uvolnovanie
+	delete[] Vs;
+	delete[] Vc;
+	delete[] Vp;
+	delete[] Vi;
+	delete[] Vl;
+	delete[] Vm;
+	delete[] Vd;
+	delete[] H;
+	delete[] Hp;
+
+	return(rv);
+}
+
 SOLUTION* solveBellmanFord(const JOB * j){
 	int * Vs = new int[j->vertNo+1];	//index kde zacina zoznam nasledovnikov vo Vi
 	int * Vc = new int[j->vertNo+1];	//pocet zaznamov vo Vi pre dany vrchol
@@ -498,6 +658,135 @@ SOLUTION* solveBellmanFordLimit(const JOB * j){
 	delete[] Vc;
 	delete[] Vp;
 	delete[] Vi;
+	delete[] Vl;
+	delete[] Vd;
+	delete[] Fp;
+	delete[] F;	
+
+	return(rv);
+}
+
+SOLUTION* solveBellmanFordGeom(const JOB * j){
+	int * Vs = new int[j->vertNo+1];	//index kde zacina zoznam nasledovnikov vo Vi
+	int * Vc = new int[j->vertNo+1];	//pocet zaznamov vo Vi pre dany vrchol
+	int * Vp = new int[j->vertNo+1];	//predchodcovia na ceste zo src
+	int * Vm = new int[j->vertNo+1];	//minimalna dlzka cesty spocitana ako eukl. vzdialenost
+	int * Vi = new int[(j->edgeNo)*2];	//zoznam naslednikov, naslednici pre i zacinaju od Vi[Vs[i]]	
+	int * Vl = new int[(j->edgeNo)*2];	//dlzky hran
+
+	int src = j->src;					//pociatok
+	int dst = j->dst;					//koniec
+
+	int cumc = 0;						//kumulativne cetnosti
+	int hilimit = j->vertNo+1;	
+	for (int rpc = 1; rpc< hilimit; rpc++ ) {
+		Vs[rpc] = cumc;					//zoznam zacina v poli Vi na indexe cumc
+		Vc[rpc] = 0;					//pocet naslednikov ulozeny pre kazdy vrchol je zatial 0
+		cumc+=j->V[rpc].deg;			//posun o stupen vrchola
+	}
+	int dx=j->V[j->dst].x, dy=j->V[j->dst].y, dz=j->V[j->dst].z;
+	int rx,ry,rz;
+	for (int rpc = 1; rpc<hilimit; rpc++){		//spocitanie minimalnych vzdialenosti
+		rx = j->V[rpc].x - dx;
+		ry = j->V[rpc].y - dy;
+		rz = j->V[rpc].z - dz;
+		Vm[rpc] = int(floor(sqrt(double(rx*rx+ry*ry+rz*rz))));
+	}
+
+	int v1,v2;	
+	hilimit=j->edgeNo;
+	for (int rpc=0; rpc < hilimit; rpc++){
+		EDGE e = j->E[rpc];
+		v1 = e.v1;
+		v2 = e.v2;
+		Vl[Vs[v1]+Vc[v1]]=e.l;			//dlzka cesty
+		Vi[Vs[v1]+Vc[v1]++]=v2;			//v1 prida do zoznamu naslednikov v2 a zvysi pocet vo Vc[v1]
+		Vl[Vs[v2]+Vc[v2]]=e.l;
+		Vi[Vs[v2]+Vc[v2]++]=v1;
+	}	
+
+	int * Vd = new int[j->vertNo+1];	//vzdialenost do vrchola z source, INT_MAX pre neprebrany	
+	int Fsize = 4*(j->vertNo+1);		//pociatocna velkost fronty
+	int * F = new int[Fsize];			//indexuje sa od 1
+	int * Fp = new int[j->vertNo+1];    //pozicia vrchola vo fronte, 0 = nieje tam	
+	hilimit=j->vertNo+1;
+	for (int rpc=0;rpc<hilimit;rpc++) { Fp[rpc] = 0; Vd[rpc] = INT_MAX; }
+	F[1] = src;							//najprv je tam len src
+	Fp[src] = 1;						// a to na pozici 1 v F1
+	Vd[src]=0;							//vzialenost do pociatku je 0
+	int Fs = 1;							//ak je fronta neprazdna, index prveho prvku
+	int Fe = 2;							//index prveho volneho miesta
+	int Fc = 1;							//pocet prvkov vo fronte	
+
+	int fvI;							//index prveho vrchola (vybraneho z fronty)
+	while(Fc){							//kym je vo fronte nejaky vrchol				
+
+		do {fvI = F[Fs]; Fs=(Fs % (Fsize-1))+1; Fc--; Fp[fvI] = 0;//zmena indexu (Fs % (Fsize-1))+1 je ok,pretoze sa indexuje od 1 po Fsize-1
+		}while ( Fc &&(!fvI));//0 su prazdne miesta
+		if (fvI==0) break;//uz tam nieje nic
+		if (Vd[fvI] + Vm[fvI] > Vd[dst]) continue;//pokracuje ak teraz cestou cez tento vrchol nemoze zlepsit
+		hilimit = Vs[fvI]+Vc[fvI];
+		for (int rpc = Vs[fvI]; rpc<hilimit; rpc++){//preberie nasledovnikov
+			if (Vd[Vi[rpc]]>Vd[fvI]+Vl[rpc]){
+				Vp[Vi[rpc]] = fvI;		//nastavi noveho predchodcu
+				Vd[Vi[rpc]] = Vd[fvI]+Vl[rpc]; //nastavi novu vzdialenost				
+				if (Fp[Vi[rpc]])		//ak je vo fronte, je najprv prepisany nulou
+					F[Fp[Vi[rpc]]] = 0;														
+				if (Fc == Fsize-1){		//uz by do fronty nevosiel
+					//prealokovat frontu
+					int * tmp = new int[Fsize*2];//raz tolko
+					int hilimit=__min(Fsize-Fs,Fc);
+					for(int rpc=0; rpc<hilimit; rpc++) tmp[rpc+1]=F[Fs+rpc];//skopiruje od Fs po koniec pola
+					int hilimit2 = Fc-hilimit;//zostava skopirovat Fc - hilimit
+					for(int rpc=0; rpc<hilimit2; rpc++) tmp[rpc+hilimit+1]=F[1+rpc];//skopiruje zvysok
+					Fs = 1;
+					Fe = Fc+1;
+					delete[] F;
+					Fsize *= 2;
+					F = tmp;
+					hilimit = Fc+1;
+					for(int rpc=1; rpc<hilimit; rpc++) Fp[F[rpc]]=rpc;//prepise zaradenie
+				}
+				F[Fe] = Vi[rpc];//prida nakoniec
+				Fp[Vi[rpc]] = Fe;//zapise kam ho zaradil
+				Fe=(Fe % (Fsize-1))+1;//posunie koniec
+				Fc++;//zvysi pocet zaznamov vo fronte
+			}
+		}		
+	}
+
+	//vytvorenie vysledku
+	SOLUTION * rv = new SOLUTION;
+	if (Vd[dst]==INT_MAX) {							//ak to dobehlo bez dosiahnutia ciela
+		rv->exists=false;
+		rv->l=0;
+		rv->V=NULL;
+		rv->vertNo=0;		
+	} else {
+		//vytvorenie navratovej hodnoty v tvare: (dlzkacesty)src,v1,v2,...,vn,dst
+		rv->exists = true;
+		rv->l = Vd[dst];
+		int i = dst,count = 1;
+		do {
+			i = Vp[i];
+			count++;
+		} while (i!=src);
+		rv->vertNo = count;
+		rv->V = new unsigned int[count];
+		unsigned int * P = rv->V;	
+		i = dst;
+		P[--count] = dst;
+		do {		
+			i = Vp[i];
+			P[--count] = i;
+		} while (i!=src);
+	}
+	//uvolnovanie
+	delete[] Vs;
+	delete[] Vc;
+	delete[] Vp;
+	delete[] Vi;
+	delete[] Vm;	
 	delete[] Vl;
 	delete[] Vd;
 	delete[] Fp;
